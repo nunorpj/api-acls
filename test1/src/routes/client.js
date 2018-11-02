7b8a3650013958988161bce081773b2973556515
nunorpj <nunorpj@gmail.com>
2018-11-02 10:01:09 +0000

    delete teste1

--- test1/src/routes/client.js
+++
@@ -1,6 +0,0 @@
-const router = module.exports = require('express').Router();
-const {checkExistance,ckeckColaborator} = require("./../middleware/client")
-const {insertClient,insertColab} = require("./../core/client")
-
-router.post("/insertclient",checkExistance,insertClient);
-router.post("/insertColab", ckeckColaborator,insertColab)
--- test1/src/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = module.exports = require('express').Router();
-const {verifyEmail,verifyRegistryData,verifyLogInData} = require("../middleware/auth")
-const {login,registry} = require("../core/auth")
-
-
-router.post("/registry",verifyRegistryData,verifyEmail,registry );
-router.post("/login", verifyLogInData,login)
